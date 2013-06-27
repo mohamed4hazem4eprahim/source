a61998b0a254fc74fb55de78b34579c20a7ccddb
Nika Khutsishvili <nkhut10@freeuni.edu.ge>
2013-06-27 23:53:57 +0400

    MainActivity.java მორჩა საწყის ეტაპზე.
    მოკლედ ასინქტასკს გადავუკეთე მისაღები პარამეტრები და მაპს გაწვდი, როგორც
    ტასკში გავწერე. ეხალ მაგალითს გამოგიგზავნი ჩემსას მეილზე, როგორ უნდა
    ამოიღო მაპიდან და მაგის მიხედვით გაგზავნო რექვესტი. მე დღეს პას

--- src/com/example/chat/MainActivity.java
+++ src/com/example/chat/MainActivity.java
@@ -1,46 +1,51 @@
 package com.example.chat;
 
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private Spinner spinner1;
+	private EditText editText1;
 	private Button button1;
+	
+	HashMap<String, Object> map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		addListenerOnSpinnerItemSelection();
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		editText1 = (EditText) findViewById(R.id.editText1);
+		button1 = (Button) findViewById(R.id.button1);
+		
+		map = new HashMap<String, Object>();
+		
 		addListenerOnButton();
 	}
 
-	public void addListenerOnSpinnerItemSelection() {
-		spinner1 = (Spinner) findViewById(R.id.spinner1);
-		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-	}
-
 	public void addListenerOnButton() {
-		spinner1 = (Spinner) findViewById(R.id.spinner1);
-		button1 = (Button) findViewById(R.id.button1);
 
 		button1.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(
-						MainActivity.this,
-						"OnClickListener : " + "\nSpinner 1 : "
-								+ String.valueOf(spinner1.getSelectedItem()),
-						Toast.LENGTH_SHORT).show();
+				String category = String.valueOf(spinner1.getSelectedItem());
+				String price = editText1.getText().toString();
+				
+				map.put("category", category);
+				map.put("price", price);
+				
+				(new AsyncTaskStuff()).execute(map);
 			}
 		});
 	}
