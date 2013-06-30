e2bb37cf2ae4cc15aba9cc06d89e624678d28f75
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-06-30 16:03:46 +0400

    categoriebis linkebis agscoreba
    gavascore categoriis linkebi

--- src/com/example/chat/AsyncTaskStuff.java
+++ src/com/example/chat/AsyncTaskStuff.java
@@ -16,14 +16,27 @@ import java.util.*;
 
 public class AsyncTaskStuff extends AsyncTask<Object, Void, List<Item>> {
 	
-	private String link="http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=4&Itemid=1&lang=ka";
 	private List<Item> items;
 	private Map <String, Object> param; 
 	private Context mContext;
+	private Map <String ,String> link;
+	private String url;
 	
 	public AsyncTaskStuff(Map<String, Object> map, Context applicationContext) {
+		link.put("kvebis obieqtebi","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=3&Itemid=1&lang=ka");
+		link.put("gartoba","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=4&Itemid=1&lang=ka");
+		link.put("janmrteloba da silamaze","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=5&Itemid=1&lang=ka");
+		link.put("turizmi da sastumroebi","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=6&Itemid=1&lang=ka");
+		link.put("bavshvebs","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=7&Itemid=1&lang=ka");
+		link.put("wignebi da jurnalebi","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=10&Itemid=1&lang=ka");
+		link.put("sayofacxovrebo teqnika","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=11&Itemid=1&lang=ka");
+		link.put("swavla da ganatleba","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=12&Itemid=1&lang=ka");
+		link.put("sxva momsaxurebebi","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=8&Itemid=1&lang=ka");
+		link.put(" sxva produqcia","http://www.hotsale.ge/index.php?option=com_sobi2&sobi2Task=rss&no_html=1&catid=37&Itemid=1&lang=ka");
 		//param = new HashMap<String, Object>();
 		param = map;
+		String category  = param.get("category").toString();
+		url = link.get(category);
 		mContext = applicationContext;
 	}
 	
@@ -32,7 +45,7 @@ public class AsyncTaskStuff extends AsyncTask<Object, Void, List<Item>> {
 	protected List<Item> doInBackground(Object... params) {
 		double price =  Double.parseDouble(param.get("price").toString());
 		DefaultHttpClient client = new DefaultHttpClient();
-		HttpGet getRequest = new HttpGet(link);
+		HttpGet getRequest = new HttpGet(url);
 		try {
 			HttpResponse response = client.execute(getRequest);
 			HttpEntity entity = response.getEntity();
